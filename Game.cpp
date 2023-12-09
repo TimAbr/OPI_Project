831012ea82d7bf4a082ba82ba6a9821fa24aaf87
IlianBuh <ilianbuhovec@gmail.com>
2023-12-09 18:31:51 +0300

    Implementation of validation of input at the first step

--- Game.cpp
+++ Game.cpp
@@ -2,78 +2,27 @@
 #include <fstream>
 #include <ctime>
 #include <Windows.h>
-
+#include <string>
 using namespace std;
 
-string Entered_Words[65];
-int Num_Word{0};
+bool InputCheckFirstStep(string UsersWord, string CorrectWord){
 
-int GetRandomNumber(int min,int max){
-    srand(time(NULL));
-    int num = min+rand()%(max-min+1);
-    return num;
-} 
+    int StartCheck{UsersWord.find_first_not_of(' ')}, EndCheck{UsersWord.find_last_not_of(' ')};
+    
+    if (EndCheck - StartCheck != size(CorrectWord) - 1){
+        return false;
+    }
+    else{
+        for (int i = 0; i < size(CorrectWord); i++)
+            if (UsersWord[StartCheck + i] != CorrectWord[size(CorrectWord) - i - 1])
+                return false;
+    }
+    return true;
 
-string Return_Word(int);
+}
 
 int main(){
-    int Letters_Amount;
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    for (int n =1;n<=10;n++){
-        cout<<" Введите количество букв в "<< (Num_Word+1) << " слове: ";
-        cin>>Letters_Amount;
-        cout<<"\n Ваше случайное слово: "<<Return_Word(Letters_Amount)<<endl;
-    }
-    cin>>Letters_Amount;
+    
     return 0;
 }
 
-string Return_Word(int letter){
-    string Word;
-    fstream file;
-    int Flag =false;
-    if ((letter >8) or (letter<5)){
-        Flag=true;
-        cout<<"\n некорректное значение";
-    }
-    while(!Flag){
-
-        int Word_Index = GetRandomNumber(1,100);
-
-        switch (letter){
-            case 5: 
-                file.open("5_letters.txt");
-                break;
-            case 6: 
-                file.open("6_letters.txt"); 
-                break;
-            case 7: 
-                file.open("7_letters.txt"); 
-                break;
-            case 8: 
-                file.open("8_letters.txt"); 
-                break;
-        }
-
-        if (file.is_open()) { 
-                Flag = true;
-                for (int lineno = 1; (lineno <= Word_Index) && getline(file,Word); lineno++);
-                for(int i = 0; i<=Num_Word; i++){ 
-                    if (Entered_Words[i] == Word){
-                        Flag=false;
-                    }
-                }
-
-        }
-        else{    
-            cout<<"Error. Cannot open file"<<endl;
-        }
-
-        file.close();
-    }
-
-    Num_Word++;
-    Entered_Words[Num_Word]=Word;
-    return Word;
-}
