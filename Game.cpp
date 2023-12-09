e6946ee5bcb8660ffc49e8963eaa7c0ab026ce7c
IlianBuh <ilianbuhovec@gmail.com>
2023-12-09 18:31:51 +0300

    Implementation of validation of input at the first step

--- Game.cpp
+++ Game.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+bool InputCheckFirstStep(string UsersWord, string CorrectWord){
+
+    int StartCheck{UsersWord.find_first_not_of(' ')}, EndCheck{UsersWord.find_last_not_of(' ')};
+    
+    if (EndCheck - StartCheck != size(CorrectWord) - 1){
+        return false;
+    }
+    else{
+        for (int i = 0; i < size(CorrectWord); i++)
+            if (UsersWord[StartCheck + i] != CorrectWord[size(CorrectWord) - i - 1])
+                return false;
+    }
+    return true;
+  
+    
+}
+
 int main(){
-    cout<<"hello world";
+    
     return 0;
 }
