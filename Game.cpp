f399443356fcb279dadc7d41a570b0b7dce487bd
IlianBuh <ilianbuhovec@gmail.com>
2023-12-10 15:45:24 +0300

    Input the second stage

--- Game.cpp
+++ Game.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <ctime>
-#include <Windows.h>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 bool InputCheckFirstStep(string UsersWord, string CorrectWord){
 
-    int StartCheck{UsersWord.find_first_not_of(' ')}, EndCheck{UsersWord.find_last_not_of(' ')};
-    
+    int StartCheck = UsersWord.find_first_not_of(' ');
+    int EndCheck = UsersWord.find_last_not_of(' ');
+
     if (EndCheck - StartCheck != size(CorrectWord) - 1){
         return false;
     }
@@ -18,11 +18,40 @@ bool InputCheckFirstStep(string UsersWord, string CorrectWord){
                 return false;
     }
     return true;
+}
 
+bool InputCheckSecondStep(string UsersLine, string GenWords[5]){
+
+    int StartCheck = UsersLine.find_first_not_of(' ');
+    vector <string> UsersEnteredWords(5);
+    for (int i = 0;i < 5; i ++){
+        if (StartCheck == -1) return false;
+        UsersEnteredWords[i] = UsersLine.substr(StartCheck,(UsersLine.find_first_of(' ', StartCheck) - StartCheck));
+        StartCheck = UsersLine.find_first_not_of(' ', UsersLine.find_first_of(' ', StartCheck));
+    }
+
+    if (StartCheck != -1) return false;
+
+    for (int i = 0;i < 5;i++){
+
+        bool flag = false;
+        for (int j = 0;!flag && j < 5;j++){
+
+            if (UsersEnteredWords.back() == GenWords[j]){
+                UsersEnteredWords.pop_back();
+                flag = true;
+            }
+
+        }
+
+        if (!flag)
+            return false;
+    }
+    
+    return true;
 }
 
 int main(){
-    
+
     return 0;
 }
-
